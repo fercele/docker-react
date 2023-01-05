52b000f01f8096151fd7808b66c6a79c4102fb3f
celestino.frc <celestino.frc@pf.gov.br>
2023-01-05 16:55:56 -0300

    pull request on test fail

--- src/App.test.js
+++ src/App.test.js
@@ -7,8 +7,8 @@ test("renders learn react link", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders learn react link", () => {
+test("This one is going to fail", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(true).toBe(false);
 });
